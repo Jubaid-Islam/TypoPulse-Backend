35e2d168ebedbb37bcc45dee72d3f0dd12ba14ef
Jubaid-Islam <jubaid20062@gmail.com>
2026-08-28 15:00:16 +0600

    update better auth trusted origin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ import { prisma } from "./prisma";
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:4000",
 
-  trustedOrigins: [process.env.APP_URL || "http://localhost:3000"],
+  trustedOrigins: ["https://typopulse-frontend.vercel.app", "http://localhost:3000"],
   advanced: {
     defaultCookieAttributes: {
       sameSite: "none",
